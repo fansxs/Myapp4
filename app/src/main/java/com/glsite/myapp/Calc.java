e66aa8479eb8aeec524a6daefdea7859055942c5
fansxs <343345220@qq.com>
2019-03-22 18:20:05 +0800

    add calc class

---
+++ app/src/main/java/com/glsite/myapp/Calc.java
@@ -0,0 +1,15 @@
+package com.glsite.myapp;
+
+public class Calc {
+    /**
+     * 两个整数想加
+     * @param a
+     *          整数1
+     * @param b
+     *          整数2
+     * @return  两个整数的和
+     */
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
